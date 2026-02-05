d94f9df94c0a99e4d69f156c1c6e8946f8b80b9b
Gordeev Pavel <gordeevpavel@MacBook-Pro-Gordeev.local>
2026-02-05 23:35:20 +0300

    feat(primitives): add HeartIcon

--- src/components/primitives/Icon/Icons/index.ts
+++ src/components/primitives/Icon/Icons/index.ts
@@ -2,3 +2,5 @@ export { DocumentIcon } from './DocumentIcon';
 export { CloseIcon } from './CloseIcon';
 export { HomeIcon } from './HomeIcon';
 export { FullscreenIcon } from './FullscreenIcon';
+export { HeartIcon } from './HeartIcon';
+export type { HeartIconProps } from './HeartIcon';
